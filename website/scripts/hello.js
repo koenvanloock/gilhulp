1353f6c2a1d9100cb33f8ed21a4df3f39882963e
koenvanloock <koentjevl@gmail.com>
2017-02-18 12:01:14 +0100

    added javascripts for angular material

--- website/scripts/hello.js
+++ website/scripts/hello.js
@@ -1,9 +1,20 @@
-var testApp = angular.module("testApp", []);
+var testApp = angular.module("testApp", ['ngMaterial']);
 
 
 testApp.controller("paginaController", ["$scope", function($scope){
 
   $scope.naam="gil";
+  
+  $scope.number=100;
+  
+  $scope.getStyle = function () {
+    return { color: "red"};
+  };
+
+  $scope.heyGil= function () {
+
+    alert("hey gil");
+  }
 
 }]);
 
